76f1ab5e0c3713640073814c9e84f5074b6e37a1
mskj-zhouyi <zhouyi@mskj.com>
2023-02-22 22:30:39 +0800

    2月22日

--- apps/basic-item/src/views/login/index.vue
+++ apps/basic-item/src/views/login/index.vue
@@ -2,50 +2,35 @@
  * @Author: 周毅
  * @Date: 2023-02-17 16:12:16
  * @LastEditors: mskj-zhouyi zhouyi@mskj.com
- * @LastEditTime: 2023-02-18 12:51:45
+ * @LastEditTime: 2023-02-22 22:27:22
  * @FilePath: /wework/apps/basic-item/src/views/login/index.vue
 -->
+
+<script setup>
+import Login from "@/components/Loding/index.vue";
+</script>
+
 <template>
-  <div className='loading'>
-    <div className='loading-main'>
-      <div className='loading-main-title'>加载中...</div>
-      <div className='loading-main-animation'>
-        <div>
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </div>
-  </div>
+  <Login />
 </template>
 
 <script>
-import { $http } from "utils";
-console.log(1234)
-const getConfig = await $http.post("/GetConfigService", {
-  keyGets: [
-    "QW_OAOUTH_URL",
-    "QW_APPID",
-    "QW_CORP_ID",
-    "QW_PAGE_SIGN",
-    "QW_SCOPE",
-    "QW_RESPONSE_TYPE",
-    "QW_AGENTID",
-    "QW_USERCHECK_AGENTID",
-    "QW_DOMAIN_APPID",
-    "CUBE_SP_NAME",
-    "QW_CODEURL",
-    "QW_TXZ_URL",
-  ]
-});
-console.log(getConfig)
-</script>
+import { urlParams, login } from "utils";
+import { mcpCodeTestUrl } from "@/constants/global.js";
+import { useUserInfoStore } from "@/stores/user.js";
+const ENV = process.env.NODE_ENV;
+const store = useUserInfoStore();
+const { mcpCode, ...restProps } = urlParams(window.location.href);
 
-<style lang="less">
-  @import "./style.less";
-</style>
+console.log("当前开发环境 - ", ENV);
+
+// 跳转腾讯授权获取 - mcpCode
+if (!mcpCode && JSON.stringify(store.userInfo) === "{}") {
+  ENV === "development" ? (window.location.href = mcpCodeTestUrl) : login();
+}
+
+// 消费 mcpCode 获取用户信息
+if (mcpCode && JSON.stringify(store.userInfo) === "{}") {
+  store.getUserInfo(mcpCode, restProps);
+}
+</script>
